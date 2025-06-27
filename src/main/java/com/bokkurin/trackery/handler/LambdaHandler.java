9c45354397b36b1ee0ddc7ed350807439dc0da9f
durururuk <147094944+durururuk@users.noreply.github.com>
2025-06-27 16:37:57 +0900

    feature/s3-interaction/ SAM 환경과 실제 Lambda 환경에 상관없이 동작하도록 구조 변경

--- src/main/java/com/bokkurin/trackery/handler/LambdaHandler.java
+++ src/main/java/com/bokkurin/trackery/handler/LambdaHandler.java
@@ -1,14 +1,13 @@
 package com.bokkurin.trackery.handler;
 
-import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import com.bokkurin.trackery.config.AwsConfiguration;
 import com.bokkurin.trackery.service.S3ActionService;
 
@@ -25,7 +24,7 @@ import software.amazon.awssdk.services.s3.S3Client;
  * -----------------------------------------------------------
  * 25. 6. 26.		durururuk		최초 생성
  */
-public class LambdaHandler implements RequestHandler<S3Event, String> {
+public class LambdaHandler implements RequestHandler<Map<String, Object>, String> {
 	private static final Logger logger = LoggerFactory.getLogger(LambdaHandler.class);
 
 	private final S3ActionService s3ActionService;
@@ -36,25 +35,36 @@ public class LambdaHandler implements RequestHandler<S3Event, String> {
 	}
 
 	@Override
-	public String handleRequest(S3Event s3Event, Context context) {
+	public String handleRequest(Map<String, Object> input, Context context) {
 		logger.info("이미지 후처리 실행");
 
 		try {
-			for (S3EventNotification.S3EventNotificationRecord notificationRecord : s3Event.getRecords()) {
-				String bucketName = notificationRecord.getS3().getBucket().getName();
-				String objectKey = notificationRecord.getS3().getObject().getKey();
+			@SuppressWarnings("unchecked")
+			List<Map<String, Object>> records = (List<Map<String, Object>>)input.get("Records");
+
+			for (Map<String, Object> record : records) {
+				@SuppressWarnings("unchecked")
+				Map<String, Object> s3 = (Map<String, Object>)record.get("s3");
+
+				@SuppressWarnings("unchecked")
+				Map<String, Object> bucket = (Map<String, Object>)s3.get("bucket");
+				String bucketName = (String)bucket.get("name");
+
+				@SuppressWarnings("unchecked")
+				Map<String, Object> object = (Map<String, Object>)s3.get("object");
+				String objectKey = (String)object.get("key");
 
 				logger.info("처리할 파일 - 버킷: {}, 키: {}", bucketName, objectKey);
 
 				byte[] imageBytes = s3ActionService.downloadImage(bucketName, objectKey);
 				logger.info("다운로드 성공 - 파일 크기: {} bytes", imageBytes.length);
-
 			}
 
 			return "SUCCESS";
-		} catch (IOException e) {
+		} catch (Exception e) {
 			logger.error("이미지 처리 실패", e);
 			throw new RuntimeException("Lambda 실행 실패", e);
 		}
 	}
+
 }
